2ed7779ed39cf03acc44c5e67a842afdaedee76a
Max <maxonchik@bk.ru>
2021-11-04 12:38:06 +0300

    13.Systemd

--- 13.Systemd/systemd.sh
+++ 13.Systemd/systemd.sh
@@ -1,10 +1,11 @@
 setenforce 0
 yum install -y epel-release
-yum install -y spawn-fcgi httpd
+yum install -y spawn-fcgi httpd php
 mv /vagrant/myunit.service /etc/systemd/system/
 mv /vagrant/myunit.timer /etc/systemd/system/
 mv /vagrant/myunit.ini /etc/sysconfig/
 mv /vagrant/spawn-fcgi.service /etc/systemd/system/
+mv /vagrant/spawn-fcgi.ini /etc/sysconfig/
 cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/1.conf
 cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/2.conf
 sed -i 's/Listen 80/Listen 8081/' /etc/httpd/conf/1.conf
@@ -12,5 +13,6 @@ sed -i 's/Listen 80/Listen 8082/' /etc/httpd/conf/2.conf
 echo "PidFile /var/run/httpd/httpd1.pid" >> /etc/httpd/conf/1.conf
 echo "PidFile /var/run/httpd/httpd2.pid" >> /etc/httpd/conf/2.conf
 systemctl start myunit.service
+systemctl start spawn-fcgi.service
 systemctl start httpd@1.service
 systemctl start httpd@2.service
